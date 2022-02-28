54f4e295a8c503b02e58a1c0a30ff392ffdafd60
Javier Acevedo <ja.acevedo12@uniandes.edu.co>
2022-02-28 13:03:16 +0100

    Turning the repo into a simple package with the network. Quality of life utils file to be included in the future.

--- CMUDeepLens/deeplens.py
+++ CMUDeepLens/deeplens.py
@@ -1,37 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 14 01:16:30 2022
-
-@author: Javier Alejandro Acevedo Barroso
-Very heavily inspired by
-https://github.com/marcopeix/Deep_Learning_AI/blob/master/4.Convolutional%20Neural%20Networks/2.Deep%20Convolutional%20Models/Residual%20Networks.ipynb
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from tensorflow.keras import layers
-from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.preprocessing import image
-# from tensorflow.keras.utils import layer_utils
-# from tensorflow.keras.utils.data_utils import get_file
-from tensorflow.keras.applications.imagenet_utils import preprocess_input
-# from tensorflow.keras.utils.vis_utils import model_to_dot
-# from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D
+from tensorflow.keras.models import Model
 import tensorflow as tf
 from tensorflow.keras.initializers import glorot_uniform
-# import scipy.misc
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-from astropy.table import Table
-from astropy.io import fits
-from os.path import join
-
-import tensorflow.keras.backend as K
-K.set_image_data_format('channels_last')
-K.set_learning_phase(1)
-#%%
 
 def identity_block(X, f, filters, stage, block):
     
@@ -48,12 +20,12 @@ def identity_block(X, f, filters, stage, block):
     # First component of main path
     X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
-    X = Activation('relu')(X)
+    X = Activation('elu')(X)
     
     # Second component of main path
     X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1, 1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
-    X = Activation('relu')(X)
+    X = Activation('elu')(X)
 
     # Third component of main path 
     X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
@@ -61,7 +33,7 @@ def identity_block(X, f, filters, stage, block):
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation
     X = Add()([X, X_shortcut])
-    X = Activation('relu')(X)
+    X = Activation('elu')(X)
     
     return X
 
@@ -84,14 +56,14 @@ def convolutional_block(X, f, filters, stage, block, s=2):
                padding='valid', name=conv_name_base + '2a',
                kernel_initializer=glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
-    X = Activation('relu')(X)
+    X = Activation('elu')(X)
     
     # Second component of main path
     X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1),
                padding='same', name=conv_name_base + '2b',
                kernel_initializer=glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
-    X = Activation('relu')(X)
+    X = Activation('elu')(X)
 
 
     # Third component of main path
@@ -106,22 +78,22 @@ def convolutional_block(X, f, filters, stage, block, s=2):
 
     # Final step: Add shortcut value to main path, and pass it through a RELU activation
     X = Add()([X, X_shortcut])
-    X = Activation('relu')(X)
+    X = Activation('elu')(X)
 
     
     return X
 
+#%%
 def DeepLens(input_shape = (44, 44, 1), classes = 2):
     
     # Define the input as a tensor with shape input_shape
     X_input = Input(input_shape)
-
     
     # Zero-Padding
     X = ZeroPadding2D((3, 3))(X_input)
     
     # Stage 1
-    # The Theano original implementation had sqrt(12/(in+out)), here is not 12 but 6.
+    # The Theano original implementation had sqrt(12/(in+out)) for the range of the initialization distribution, here is not 12 but 6.
     X = Conv2D(32, (7, 7), strides = (2, 2), name = 'conv1',
                kernel_initializer = glorot_uniform(seed=0))(X) 
     X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
@@ -129,76 +101,45 @@ def DeepLens(input_shape = (44, 44, 1), classes = 2):
     X = MaxPooling2D((3, 3), strides=(2, 2))(X)
 
     # Stage 2
-    # X = convolutional_block(X, f = 3, filters = [16, 16, 32], stage = 2, block='a', s = 1)
     X = identity_block(X, 3, [16, 16, 32], stage=2, block='a')
     X = identity_block(X, 3, [16, 16, 32], stage=2, block='b')
     X = identity_block(X, 3, [16, 16, 32], stage=2, block='c')
 
     # Stage 3
-    X = convolutional_block(X, f=3, filters=[32, 32, 64], stage=3, block='a', s=2)
+    X = convolutional_block(X, f=3, filters=[32, 32, 64],
+                            stage=3, block='a', s=2)
     X = identity_block(X, 3, [32, 32, 64], stage=3, block='b')
     X = identity_block(X, 3, [32, 32, 64], stage=3, block='c')
+
+    # Stage 4
+    X = convolutional_block(X, f=3, filters=[64, 64, 128],
+                            stage=4, block='a', s=2)
+    X = identity_block(X, 3, [64, 64, 128], stage=4, block='b')
+    X = identity_block(X, 3, [64, 64, 128], stage=4, block='c')
+    
+    # Stage 5
+    filters = [128, 128, 256]
+    X = convolutional_block(X, f=3, filters=filters,
+                            stage=5, block='a', s=2)
+    X = identity_block(X, 3, filters, stage=5, block='b')
+    X = identity_block(X, 3, filters, stage=5, block='c')
+    
+    # Stage 6
+    filters = [256, 256, 512]
+    X = convolutional_block(X, f=3, filters=filters,
+                            stage=6, block='a', s=2)
+    X = identity_block(X, 3, filters, stage=6, block='b')
+    X = identity_block(X, 3, filters, stage=6, block='c')
 
     # AVGPOOL
     X = AveragePooling2D(pool_size=(2,2), padding='same')(X)
 
     # Output layer
     X = Flatten()(X)
-    X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Dense(1, activation='sigmoid', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
     
     
     # Create model
     model = Model(inputs = X_input, outputs = X, name='DeepLens')
 
     return model
-
-#%%
-import h5py
-def load_dataset():
-    train_dataset = h5py.File('Data/train_signs.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-
-    test_dataset = h5py.File('Data/test_signs.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
-
-def convert_to_one_hot(Y, C):
-    Y = np.eye(C)[Y.reshape(-1)].T
-    return Y
-
-#%%
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
-# Normalize image vectors
-X_train = X_train_orig/255.
-X_test = X_test_orig/255.
-
-# Convert training and test labels to one hot matrices
-Y_train = convert_to_one_hot(Y_train_orig, 6).T
-Y_test = convert_to_one_hot(Y_test_orig, 6).T
-
-print ("number of training examples = " + str(X_train.shape[0]))
-print ("number of test examples = " + str(X_test.shape[0]))
-print ("X_train shape: " + str(X_train.shape))
-print ("Y_train shape: " + str(Y_train.shape))
-print ("X_test shape: " + str(X_test.shape))
-print ("Y_test shape: " + str(Y_test.shape))
-
-#%%
-model = DeepLens(input_shape = (64, 64, 3), classes = 6)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-#%%
-# model.fit(X_train, Y_train, epochs = 2, batch_size = 32)
-# #%%
-# preds = model.evaluate(X_test, Y_test)
-# print ("Loss = " + str(preds[0]))
-# print ("Test Accuracy = " + str(preds[1]))
-#%%
-# model.summary()
